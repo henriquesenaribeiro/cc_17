a96fa77edc8abe8728ea4802195b5b866346cf89
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-03-30 23:48:16 -0300

    Task 2: Created Sales Rep Class

--- cc_17.js
+++ cc_17.js
@@ -19,7 +19,32 @@ class Customer {
   }
   
   // Create a customer and log total spent
-  const customer1 = new Customer("Fabiana", "fabiana@usf.edu");
+  const customer1 = new Customer("Henry", "Henry@usf.edu");
   customer1.addPurchase(120);
   customer1.addPurchase(80);
   console.log(`${customer1.name}'s Total Spent: $${customer1.getTotalSpent()}`);
+
+// Task 2: Created Sales Rep Class
+class SalesRep {
+    constructor(name) {
+      this.name = name;
+      this.clients = [];
+    }
+  
+    // Add a customer object to the clients list
+    addClient(customer) {
+      this.clients.push(customer);
+    }
+  
+    // Find a customer by name and return their total spent
+    getClientTotal(name) {
+      const client = this.clients.find(c => c.name === name);
+      return client ? client.getTotalSpent() : 0;
+    }
+  }
+  
+  // Create sales rep and add clients
+  const rep = new SalesRep("Andre");
+  rep.addClient(customer1);
+  console.log(`${rep.name}'s Clients:`, rep.clients.map(c => c.name));
+  console.log(`Total spent by Henry: $${rep.getClientTotal("Henry")}`);
